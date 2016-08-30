7db0a63a5678bc31d353dd198d90c410bad1b5d6
jacqschweiger <jacqschweiger@gmail.com>
2016-08-30 05:53:44 -0400

    Done

---
+++ Strings and Integers.playground/Contents.swift
@@ -0,0 +1,21 @@
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type String
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type String
+
+let numberOfCountriesInAfrica = 54
+
+let sixthPlanet = "Saturn"
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet).")
+
+
+ 
